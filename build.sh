6ac7d12cb2e4f093a358d3e573114b61c323fec7
Martin Unzner <martin.u@posteo.de>
2020-01-30 13:55:51 +0100

    abort on formatting changes

--- build.sh
+++ build.sh
@@ -3,6 +3,10 @@ set -o errexit
 
 echo "========== FORMAT CODE =========="
 find . -type f \( -name '*.c' -o -name '*.h' \) -exec clang-format -i {} \; -print
+if [ -n "$(git status --porcelain)" ]; then
+    echo "There were errors in the formatting. Aborting build."
+    exit 1
+fi
 echo "============= BUILD ============="
 mkdir -p build
 cd build
