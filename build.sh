7aeafd5fbd79fe5f94f01d249f555e4785ff418b
Martin Unzner <martin.u@posteo.de>
2020-01-30 13:17:56 +0100

    ensure debug symbols are present

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ find . -type f \( -name '*.c' -o -name '*.h' \) -exec clang-format -i {} \; -pri
 echo "============= BUILD ============="
 mkdir -p build
 cd build
-cmake ..
+cmake -DCMAKE_BUILD_TYPE=Debug ..
 make
 echo "============= TEST =============="
 valgrind --leak-check=full --error-exitcode=1 test/callasof_test
